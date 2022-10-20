c8a774cd19c6a3a4f34cb323483f5ed40f73ed7e
Chema Pramos <jmperezra@gmail.com>
2022-10-20 17:19:33 +0200

    Añadido diagrama de clases

--- src/com/jmperezra/pccomponentes/GraphicCard.kt
+++
@@ -1,4 +0,0 @@
-package com.jmperezra.pccomponentes
-
-class GraphicCard {
-}
--- src/com/jmperezra/pccomponentes/Main.java
+++ src/com/jmperezra/pccomponentes/Main.java
@@ -6,11 +6,6 @@ public class Main {
         System.out.println("Iniciando...");
 
         Procesador amd = new Procesador();
-
-        /**
-         * Código a ejecutar por un administrativo de la empresa.
-         * Código para introducir información de procesadores.
-         */
         amd.setId(1);
         amd.setMarca("Raizen 6400");
         amd.setFabricante("AMD");
@@ -18,14 +13,5 @@ public class Main {
         System.out.println(amd.getId());
         System.out.println(amd.getMarca());
         System.out.println(amd.getFabricante());
-
-        Procesador pentium = new Procesador();
-        pentium.setId(2);
-        pentium.setMarca("i7");
-        pentium.setFabricante("Intel");
-
-        System.out.println(pentium.getId());
-        System.out.println(pentium.getMarca());
-        System.out.println(pentium.getFabricante());
     }
 }
